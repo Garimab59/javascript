fd7c399e589190c36ad1e15431e2284557329a39
garima Bhardwaj <94923976+Garimab59@users.noreply.github.com>
2025-05-03 18:46:00 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "garima@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2      not allowed
+
+
+accountEmail = "fewfsf@g,ail.com"
+accountPassword = "243143"
+accountCity = "Jhansi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all js code as newer version 
+// alert(3 + 3).   this is old version we will use another syntax in node js 
+
+
+//code readablity is high
+console.log()
+
+let name = "abcd"
+// number = 2 to power 53
+// BigInt
+// String
+// Boolean = true/false
+// null = standalone value
+// undefined
+// symbol = unique
+
+// object 
+
+console.log(typeof "abcd");
+console.log(typeof undefined);  // undefined type
+console.log(typeof null);  // object type
